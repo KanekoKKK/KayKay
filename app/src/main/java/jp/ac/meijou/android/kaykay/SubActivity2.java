ef3e7007e7845f5c341e5994c06c4c0a146ef089
kou0612 <aichitead24409@gmail.com>
2024-12-11 11:56:46 +0900

    タイトル画面、獲得画面の追加

--- app/src/main/java/jp/ac/meijou/android/kaykay/SubActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/kaykay/SubActivity2.java
@@ -18,5 +18,34 @@ public class SubActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub2); // 遷移後のレイアウトを指定
 
-    }
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SubActivity2.this, SubActivity.class);
+                startActivity(intent);
+            }
+
+    });
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SubActivity2.this, SubActivity.class);
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.button5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SubActivity2.this, SubActivity.class);
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.button6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SubActivity2.this, SubActivity.class);
+                startActivity(intent);
+            }
+        });
+}
 }
