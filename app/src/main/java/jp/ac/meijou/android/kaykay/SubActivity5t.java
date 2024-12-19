e066176fd963e2f2b5c659455a056b23894f8fa3
KanekoKKK <231205066@ccmailg.meijo-u.ac.jp>
2024-12-19 13:04:30 +0900

    素材の選択機能

--- app/src/main/java/jp/ac/meijou/android/kaykay/SubActivity5t.java
+++ app/src/main/java/jp/ac/meijou/android/kaykay/SubActivity5t.java
@@ -1,8 +1,12 @@
 package jp.ac.meijou.android.kaykay;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,5 +30,34 @@ public class SubActivity5t extends AppCompatActivity {
         setupButtonClickListener(R.id.button5, SubActivity4.class);
         setupButtonClickListener(R.id.button6, SubActivity3.class);
         setupButtonClickListener(R.id.button9,SubActuvity7.class);
+
+        //素材の配列
+        ImageButton items[] = {
+                findViewById(R.id.image16),findViewById(R.id.image17),findViewById(R.id.image18),findViewById(R.id.image19),
+                findViewById(R.id.image20),findViewById(R.id.image21),findViewById(R.id.image22),findViewById(R.id.image23),
+                findViewById(R.id.image24),findViewById(R.id.image25),findViewById(R.id.image26),findViewById(R.id.image27),
+                findViewById(R.id.image28),findViewById(R.id.image29),findViewById(R.id.image30),findViewById(R.id.image31),
+        };
+        boolean[] isSelected = new boolean[items.length];
+
+        for (int i = 0; i < items.length; i++) {
+            final int index = i;
+            items[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    isSelected[index] = !isSelected[index];
+                    itemSelected(items[index],isSelected[index]);
+                }
+            });
+        }
+
+
+    }
+    private void itemSelected(ImageButton button, boolean isSelected) {
+        if (isSelected) {
+            button.setBackgroundColor(Color.parseColor("#FF0000")); // 選択状態なら半透明
+        } else {
+            button.setBackgroundColor(Color.parseColor("#52FFFFFF"));
+        }
     }
 }
