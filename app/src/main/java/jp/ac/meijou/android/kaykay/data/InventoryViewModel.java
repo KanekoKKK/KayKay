872fc8014dfb5980576e320c714c7fe003f9bbc6
Zen Kikuchi <231205075@ccmailg.meijo-u.ac.jp>
2024-12-20 22:25:36 +0900

    InventoryViewModel.java InventoryのViewModelを実装

---
+++ app/src/main/java/jp/ac/meijou/android/kaykay/data/InventoryViewModel.java
@@ -0,0 +1,52 @@
+package jp.ac.meijou.android.kaykay.data;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class InventoryViewModel extends AndroidViewModel {
+    private InventoryRepository mRepository;
+    private final LiveData<List<Inventory>> mAllItems;
+    private final LiveData<List<Dagashi>> mExtractableItems;
+    private final LiveData<List<Dagashi>> mMaterialItems;
+
+    // コンストラクタ
+    public InventoryViewModel(Application application) {
+        super(application);
+        mRepository = new InventoryRepository(application);
+        mExtractableItems = mRepository.getExtractableItems();
+        mMaterialItems = mRepository.getMaterialItems();
+        mAllItems = mRepository.getAllItems();
+    }
+
+    public void insertItem(Inventory item) {
+        mRepository.insertItem(item);
+    }
+
+    public void updateItem(Inventory item) {
+        mRepository.updateItem(item);
+    }
+
+    public void deleteItem(Inventory item) {
+        mRepository.deleteItem(item);
+    }
+
+    public LiveData<List<Dagashi>> getExtractableItems() {
+        return mRepository.getExtractableItems();
+    }
+
+    public LiveData<List<Dagashi>> getMaterialItems() {
+        return mRepository.getMaterialItems();
+    }
+
+    public LiveData<Inventory> getItem(int itemIndex) {
+        return mRepository.getItem(itemIndex);
+    }
+
+    public LiveData<List<Inventory>> getAllItems() {
+        return mAllItems;
+    }
+}
