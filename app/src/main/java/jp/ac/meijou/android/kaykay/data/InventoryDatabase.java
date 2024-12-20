6683c579d244132bd57eb1c9497accddf91ea1f6
Zen Kikuchi <231205075@ccmailg.meijo-u.ac.jp>
2024-12-20 22:24:25 +0900

    InventoryRepository.java リポジトリを実装
    InventoryDatabase.java
    databaseWriteExecutorを導入，リポジトリのInsert, Update, Deleteに使用

--- app/src/main/java/jp/ac/meijou/android/kaykay/data/InventoryDatabase.java
+++ app/src/main/java/jp/ac/meijou/android/kaykay/data/InventoryDatabase.java
@@ -6,12 +6,17 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Database(entities = {Dagashi.class, Inventory.class}, version = 1, exportSchema = false)
 public abstract class InventoryDatabase extends RoomDatabase {
 
     public abstract InventoryDao inventoryDao();
 
     private static volatile InventoryDatabase instance;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
     public static InventoryDatabase getDatabase(final Context context) {
         if (instance == null) {
