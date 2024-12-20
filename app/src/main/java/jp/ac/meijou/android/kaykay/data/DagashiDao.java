1f93bab1349049fadc9bc59ff1b9a7b50520d764
Zen Kikuchi <231205075@ccmailg.meijo-u.ac.jp>
2024-12-20 19:40:36 +0900

    DagashiのDAOにInsertを追加

--- app/src/main/java/jp/ac/meijou/android/kaykay/data/DagashiDao.java
+++ app/src/main/java/jp/ac/meijou/android/kaykay/data/DagashiDao.java
@@ -1,6 +1,7 @@
 package jp.ac.meijou.android.kaykay.data;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
 import androidx.room.Query;
 
 import java.util.List;
@@ -9,6 +10,9 @@ import kotlinx.coroutines.flow.Flow;
 
 @Dao
 public interface DagashiDao {
+    @Insert
+    void insertDagashi(Dagashi dagashi);
+
     @Query("SELECT * FROM dagashi WHERE dagashi_id = :dagashiId")
     public Dagashi getDagashi(String dagashiId);
 
