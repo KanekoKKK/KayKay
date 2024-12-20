a9128ce129dec2473c84a793d8fab1a10743ce60
Zen Kikuchi <231205075@ccmailg.meijo-u.ac.jp>
2024-12-20 22:21:41 +0900

    InventoryDao.java DAOの返り値の型をFlowからLiveDataに変更

--- app/src/main/java/jp/ac/meijou/android/kaykay/data/InventoryDao.java
+++ app/src/main/java/jp/ac/meijou/android/kaykay/data/InventoryDao.java
@@ -1,5 +1,6 @@
 package jp.ac.meijou.android.kaykay.data;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -8,8 +9,6 @@ import androidx.room.Transaction;
 import androidx.room.Update;
 
 import java.util.List;
-
-import kotlinx.coroutines.flow.Flow;
 
 @Dao
 public interface InventoryDao {
@@ -24,15 +23,15 @@ public interface InventoryDao {
 
     @Transaction
     @Query("SELECT * FROM dagashi AS t1 INNER JOIN inventory AS t2 ON t1.dagashi_id = t2.item_id WHERE t1.dagashi_type = 0")
-    List<Dagashi> getExtractableItems();
+    LiveData<List<Dagashi>> getExtractableItems();
 
     @Transaction
     @Query("SELECT * FROM dagashi AS t1 INNER JOIN inventory AS t2 ON t1.dagashi_id = t2.item_id WHERE t1.dagashi_type = 2")
-    List<Dagashi> getMaterialItems();
+    LiveData<List<Dagashi>> getMaterialItems();
 
-    @Query("SELECT item_amount FROM inventory WHERE item_index = :itemIndex")
-    int getItemAmount(String itemIndex);
+    @Query("SELECT * FROM inventory WHERE item_index = :itemIndex")
+    LiveData<Inventory> getItem(int itemIndex);
 
     @Query("SELECT * FROM inventory")
-    List<Inventory> getAllItems();
+    LiveData<List<Inventory>> getAllItems();
 }
