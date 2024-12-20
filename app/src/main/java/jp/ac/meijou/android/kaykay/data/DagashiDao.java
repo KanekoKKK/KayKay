66b3ee1cb56fc2b05f1d6406be74583ac7465708
Zen Kikuchi <231205075@ccmailg.meijo-u.ac.jp>
2024-12-20 19:13:48 +0900

    Daoの返り値をFlowでなくした

--- app/src/main/java/jp/ac/meijou/android/kaykay/data/DagashiDao.java
+++ app/src/main/java/jp/ac/meijou/android/kaykay/data/DagashiDao.java
@@ -10,8 +10,8 @@ import kotlinx.coroutines.flow.Flow;
 @Dao
 public interface DagashiDao {
     @Query("SELECT * FROM dagashi WHERE dagashi_id = :dagashiId")
-    public Flow<Dagashi> getDagashi(String dagashiId);
+    public Dagashi getDagashi(String dagashiId);
 
     @Query("SELECT * FROM dagashi ORDER BY dagashi_id")
-    public Flow<List<Dagashi>> getAllDagashi();
+    public List<Dagashi> getAllDagashi();
 }
--- app/src/main/java/jp/ac/meijou/android/kaykay/data/InventoryDao.java
+++ app/src/main/java/jp/ac/meijou/android/kaykay/data/InventoryDao.java
@@ -9,6 +9,8 @@ import androidx.room.Update;
 
 import java.util.List;
 
+import kotlinx.coroutines.flow.Flow;
+
 @Dao
 public interface InventoryDao {
     @Insert
